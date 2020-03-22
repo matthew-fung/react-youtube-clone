cd3c065a71ec25e198aac686c8995fab834f050c
Matthew Fung <matthewfung@Matthews-MacBook-Pro.local>
2020-03-22 17:39:30 -0400

    added card styling for video list

--- reactyoutubeclone/src/components/VideoItem.js
+++ reactyoutubeclone/src/components/VideoItem.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 export const VideoItem = ({ video }) => {
   return (
-    <>
-      <h1>{video.snippet.title}</h1>
-      <img src={video.snippet.thumbnails.default.url} />
-    </>
+    <div class="ui card">
+      <div class="image">
+        <img src={video.snippet.thumbnails.medium.url} />
+      </div>
+      <div class="content">
+        <a class="header">{video.snippet.title}</a>
+        <div class="meta">
+          <span class="date">{video.snippet.publishedAt}</span>
+        </div>
+        <div class="description">{video.snippet.description}</div>
+      </div>
+    </div>
   )
 }
